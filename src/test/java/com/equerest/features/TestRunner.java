5d4b19b4975b198d3610fec4e889baded8e28441
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 13:39:06 +0200

    ji

---
+++ src/test/java/com/equerest/features/TestRunner.java
@@ -0,0 +1,37 @@
+package com.equerest.features;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.*;
+
+@CucumberOptions(
+    features = "src/test/resourses/features",
+    glue = {"stepdefs"},
+    tags = {~@Ignore},
+    format ={
+        "pretty",
+        "html:target/cucumber-reports/cucumber-pretty",
+        "json:target/cucmber-reports/CucumberTestReport.json",
+        "rerun:target/cucumber-reports/rerun-reports/rerun.txt"
+        })
+public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider ="features");
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+@DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+}
+@AfterClass(alwaysRun = true)
+    public void tearDOwnClass() throws Exception {
+        testNGCucumberRunner.finish();
+}
+}
