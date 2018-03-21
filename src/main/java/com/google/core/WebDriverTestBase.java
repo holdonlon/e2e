6202e281532434a7a003d50e629e8fb77fd6c54d
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 21:57:05 +0200

    WebDriver

--- src/main/java/com/google/core/WebDriverTestBase.java
+++ src/main/java/com/google/core/WebDriverTestBase.java
@@ -1,17 +1,61 @@
 package com.google.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import static com.google.core.BrowsersEnum.CHROME;
+import static com.google.core.BrowsersEnum.EDGE;
+import static com.google.core.PropertiesCache.getProperty;
+import static sun.rmi.rmic.newrmic.Constants.REMOTE;
 
 @Listeners({com.google.core.TestListener.class})
 public class WebDriverTestBase {
+
+    protected WebDriver driver;
+    protected BrowsersEnum browsersEnum.valueOf(System.getProperty("browser", getProperty("defaultbrowser")));
+
+    private DesiredCapabilities setDesiredCapabilities(String platform, String remoteBrowser){
+    DesiredCapabilities caps = new DesiredCapabilities();
+    if (platform.equals(IgnoreCase(Platform.WIN10.name))) {
+        caps.setPlatform(Platform.WIN10);
+        caps.setBrowserName(remoteBrowser);
+    }
+
+    return caps;
+    }
+
+    @Parameters({"platform", "remoteBrowser"})
+    @BeforeClass
+    public void SetUp(@Optional String platform, @Optional String remoteBrowser) throws MailformedURLException {
+        switch(browser) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case REMOTE:
+                DesiredCapabilities caps = setDesiredCapabilities(platform, remoteBrowser);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+                break;
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(long.paserLong(getProperty("wait.implicit")),)
+    }
     public WebDriver driver;
     @BeforeClass
     public void setUp(){
