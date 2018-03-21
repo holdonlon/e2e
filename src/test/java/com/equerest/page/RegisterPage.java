5530790e86424eefd19c71eb55f1e2f0cb715e77
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 13:40:01 +0200

    ji

---
+++ src/test/java/com/equerest/page/RegisterPage.java
@@ -0,0 +1,20 @@
+package bdd.com.equerest.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegisterPage {
+
+    private WebDriver webDriver;
+
+
+    public RegisterPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void fillFIO(String firstName, String lastName){
+        WebElement fio = webDriver.findElement(By.id("fio"));
+        fio.sendKeys(firstName + " " + lastName);
+    }
+}
