74876a140c4e30fdab285a0a6df12749589e4c94
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 20:08:35 +0200

    SearchResultPage

--- src/main/java/selenide/SearchResultPage.java
+++ src/main/java/selenide/SearchResultPage.java
@@ -1,17 +1,22 @@
 package selenide;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class SearchResultPage {
-    public SelenideElement searchfield =  $(By.id("lst-ib"));
-    public void isvisible(){
-        searchfield.should(Condition.visible);
+    private SelenideElement searchfield =  $(By.id("//*[@id=\\\"rso\\\"]/div/div/div[1]/div/div/h3/a"));
+    private ElementsCollection resultLinks = $$(By.xpath("//*[@id=\\\"rso\\\"]/div/div/div/div/div/h3/a"));
+
+    public SelenideElement getResultLink() {
+        return resultLink;
     }
 
-    public String text() {
-        return searchfield.getValue();
+    public ElementsCollection getResultLinks() {
+        return resultLinks;
     }
 }
