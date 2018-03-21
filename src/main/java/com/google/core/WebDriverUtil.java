0e03f200d993946cceca9b5290cedf6ce341493b
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 13:38:41 +0200

    ji

---
+++ src/main/java/com/google/core/WebDriverUtil.java
@@ -0,0 +1,21 @@
+package com.google.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebDriverUtil {
+
+    protected WebDriver driver;
+    private WebDriverWait webDriverWait;
+
+    public WebDriverUtil(WebDriver driver);
+    this.driver = driver;
+    webDriverWait = new WebDriverWait(this.driver, 10);
+}
+
+public WebElement waitFor(ExpectedCondition<WebElement> expectedCondition) {
+
+    return webDriverWait.until(expectedCondition);
+}
