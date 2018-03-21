271bf078c4d9f7a23b2db27a089940c80dfe5292
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 20:09:02 +0200

    SelenideTestBase

--- src/main/java/com/google/core/SelenideTestBase.java
+++ src/main/java/com/google/core/SelenideTestBase.java
@@ -3,6 +3,7 @@ package com.google.core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.EdgeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterClass;
@@ -10,6 +11,7 @@ import org.testng.annotations.BeforeClass;
 
 import static com.codeborne.selenide.WebDriverRunner.FIREFOX;
 import static org.openqa.selenium.remote.BrowserType.CHROME;
+import static org.openqa.selenium.remote.BrowserType.EDGE;
 
 public class SelenideTestBase {
     private String browser = System.getProperty("browser", CHROME);
@@ -26,6 +28,9 @@ public class SelenideTestBase {
             case FIREFOX:
                     FirefoxDriverManager.getInstance().setup();
                     break;
+            case EDGE:
+                EdgeDriverManager.getInstance().setup();
+                break;
 
         }
 
