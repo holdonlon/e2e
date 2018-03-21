290cf4bbf6bfe9bb0541f7f660e600a0c2035409
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 21:17:06 +0200

    SearchGoogleTest

--- src/test/java/com/google/SearchGoogleTest.java
+++ src/test/java/com/google/SearchGoogleTest.java
@@ -6,14 +6,22 @@ import com.google.pages.GoogleSearchPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 
 public class SearchGoogleTest extends WebDriverTestBase {
     private By searchFieldLocator = By.xpath("//*[@id=\"lst-ib\"]");
     private By linkLocator = By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/h3/a");
+    private By searchButtonLocator = By.cssSelector("input[name='btnK']);
     private WebElement searchField;
     private WebElement linkField;
+    private List<WebElement> resultList;
+    private WebElement searchButton;
     private String searchData = "Selenium";
 
 
@@ -25,15 +33,23 @@ public class SearchGoogleTest extends WebDriverTestBase {
         searchField.submit();
         //linkField = driver.findElement(linkLocator);
         //Assert.assertTrue(linkField.getText().contains(searchData));
+        for(WebElement element : resultList) {
+            assert.assertTrue(element.getText().toLowerCase().contains(searchData));
+        }
+        driver.quit();
 
     }
 
    @Test
-    public void searchGooglePageObjectTet() {
+    public void searchGooglePageObjectTest() {
         driver.get("https://www.google.com.ua");
         GoogleSearchPage searchPage = PageFactory.initElements(driver,GoogleSearchPage.class);
         searchPage.searchFor(searchData);
         GoogleResultPage resultPage = new GoogleResultPage(driver);
+        linkField = resultPage.findLink();
+        String result = linkField.getText().toLowerCase();
+        Assert.assertTrue(linkField.getText().toLowerCase().contains(searchData));
+        driver.close();
         //Assert.assertTrue(resultPage.findLink().getText().contains(searchData));
     }
 
