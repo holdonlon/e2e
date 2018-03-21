554fc61c4bf070c9f55aa5f092a5a2ffb81ebc10
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 13:40:17 +0200

    ji

--- src/main/java/com/google/pages/GoogleResultPage.java
+++ src/main/java/com/google/pages/GoogleResultPage.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class GoogleResultPage {
 
     private WebDriver driver;
     private By linkLocator = By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/h3/a");
     private WebElement linkField;
+    private By linksLocator = By.xpath("//*[@id=\\\"rso\\\"]/div/div/div/div/div/h3/a");
+    private List<WebElement> resultList;
 
     public GoogleResultPage(WebDriver driver) {
         this.driver = driver;
@@ -17,6 +21,10 @@ public class GoogleResultPage {
     public WebElement findLink() {
         linkField = driver.findElement(linkLocator);
         return linkField;
+    }
+    public List findElements(){
+        resultList = driver.findElements(linkLocator);
+        return resultList;
     }
 
 }
