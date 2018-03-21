48b400d58e34156e5dff2ffd5a3a8bf2dc29c039
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-21 13:39:46 +0200

    ji

--- src/main/java/selenide/SearchPage.java
+++ src/main/java/selenide/SearchPage.java
@@ -2,13 +2,15 @@ package selenide;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class SearchPage {
-    public SelenideElement search = $(By.name("q"));
+    private SelenideElement search = $(By.name("q"));
 
+    @Step("Searches for {0} text")
     public SearchPage searchFor(String searchText) {
         Selenide.open("https://www.google.com");
         search.val(searchText).submit();
