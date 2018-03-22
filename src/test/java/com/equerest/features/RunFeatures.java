2c73b437b13aa2bf6af06bdb313c8511292681a3
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-03-22 08:20:13 +0200

    RunFeature

--- src/test/java/com/equerest/features/RunFeatures.java
+++ src/test/java/com/equerest/features/RunFeatures.java
@@ -3,7 +3,7 @@ package com.equerest.features;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/java/com/equerest/features", format = {"pretty",
+@CucumberOptions(features = "src/test/java/com/equerest/bdd.pages.features", format = {"pretty",
         "html:target/site/cucumber-pretty"})
 public class RunFeatures extends AbstractTestNGCucumberTests {
 }
